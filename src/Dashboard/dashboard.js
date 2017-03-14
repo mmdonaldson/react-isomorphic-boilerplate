afb7b9a2e7fa8d00c57805d6ecbdccc2c8c738d6
Mitchell Donaldson <mitchell@gorepp.com>
2017-03-15 08:28:21 +1100

    Tidy code, add hot-reloading

--- src/Dashboard/dashboard.js
+++ src/Dashboard/dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Dashboard = () => (
   <main className="Dashboard">
-    <h2>Dashboard</h2>
+    <h2>Dashboard!!!!!!!!!!!</h2>
   </main>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,62 @@
-var path = require('path');
-var webpack = require('webpack');
-var embedFileSize = 50000;
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'source-map',
   entry: [
-    // 'webpack-hot-middleware/client',
-    path.join(__dirname, 'src/main.js')
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/main.js'),
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: 'bundle.js',
   },
   plugins: [
-    // new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
-      '__DEV__': JSON.stringify(process.env.NODE_ENV)
-    })
+      __DEV__: JSON.stringify(process.env.NODE_ENV),
+    }),
   ],
   module: {
     loaders: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.json?$/,
-        loader: 'json'
+        loader: 'json',
       },
-      { test: /(\.css|\.less)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'less-loader?sourceMap'] },
-      { test: /\.svg/,
-        loader: 'url-loader?limit=' + embedFileSize + '&mimetype=image/svg+xml'
+      {
+        test: /(\.css|\.less)$/,
+        loaders: ['style-loader', 'css-loader?sourceMap', 'less-loader?sourceMap'],
       },
-      { test: /\.png$/,
-        loader: 'url-loader?limit=' + embedFileSize + '&mimetype=image/png'
+      {
+        test: /\.svg/,
+        loader: 'url-loader?mimetype=image/svg+xml',
       },
-      { test: /\.jpg/,
-        loader: 'url-loader?limit=' + embedFileSize + '&mimetype=image/jpeg'
+      {
+        test: /\.png$/,
+        loader: 'url-loader?mimetype=image/png',
       },
-      { test: /\.gif/,
-        loader: 'url-loader?limit=' + embedFileSize + '&mimetype=image/gif'
+      {
+        test: /\.jpg/,
+        loader: 'url-loader?mimetype=image/jpeg',
+      },
+      {
+        test: /\.gif/,
+        loader: 'url-loader?mimetype=image/gif',
       },
       {
         test: /\.(otf|eot|ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=' + embedFileSize
-      }
-    ]
+        loader: 'url-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.css']
+    extensions: ['.js', '.jsx', '.css'],
   },
-}
+};
